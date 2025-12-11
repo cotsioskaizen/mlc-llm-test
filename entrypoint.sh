409527128ebb96cf9134a8bea0bde5488e60b42d
Constantinos Constantinides <cotsioslord@hotmail.com>
2025-12-11 23:57:40 +0200

    [FIX] Added custom cmake file to skip input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+
 . /opt/conda/etc/profile.d/conda.sh
 echo "conda activate mlc-build" >> ~/.bashrc
 
@@ -10,8 +11,22 @@ if [[ "$1" == "build" ]]; then
     cd mlc-llm-test
 
     mkdir -p build && cd build
-    python ../cmake/gen_cmake_config.py
-
+    CONFIG_CMAKE_PATH="../cmake/config.cmake"
+if [ ! -f "$CONFIG_CMAKE_PATH" ]; then
+    cat > "$CONFIG_CMAKE_PATH" <<EOF
+set(TVM_SOURCE_DIR 3rdparty/tvm)
+set(CMAKE_BUILD_TYPE RelWithDebInfo)
+set(USE_CUDA OFF)
+set(USE_CUTLASS OFF)
+set(USE_CUBLAS OFF)
+set(USE_ROCM OFF)
+set(USE_VULKAN ON)
+set(USE_METAL OFF)
+set(USE_OPENCL OFF)
+set(USE_OPENCL_ENABLE_HOST_PTR OFF)
+EOF
+fi
+    
     cmake .. && make -j$(nproc) && cd ..
     
     python setup.py bdist_wheel
